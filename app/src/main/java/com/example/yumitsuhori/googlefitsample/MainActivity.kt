01b42f00c4e8590e017ab2e148c7a6ef72207b5e
三堀　裕 <yumitsuhori@sanhorihiroshi-no-MacBook-Pro.local>
2018-02-11 21:14:48 +0900

    歩数データ取得とmenu追加

--- app/src/main/java/com/example/yumitsuhori/googlefitsample/MainActivity.kt
+++ app/src/main/java/com/example/yumitsuhori/googlefitsample/MainActivity.kt
@@ -5,19 +5,19 @@ import android.content.Intent
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import android.util.Log
+import android.view.Menu
+import android.view.MenuItem
 import com.google.android.gms.auth.api.signin.GoogleSignIn
 import com.google.android.gms.fitness.Fitness
 import com.google.android.gms.fitness.FitnessOptions
 import com.google.android.gms.fitness.data.DataType
-import com.google.android.gms.fitness.request.DataReadRequest
-import com.google.android.gms.tasks.Tasks
-import java.util.*
-import java.util.concurrent.TimeUnit
+import com.google.android.gms.fitness.data.Field
 
 
 class MainActivity : AppCompatActivity() {
 
     private val GOOGLE_FIT_PERMISSIONS_REQUEST_CODE = 1
+    private val TAG = "GoogleFitLog"
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -28,7 +28,6 @@ class MainActivity : AppCompatActivity() {
                 .addDataType(DataType.AGGREGATE_STEP_COUNT_DELTA, FitnessOptions.ACCESS_READ)
                 .build()
 
-        val hoge = GoogleSignIn.getLastSignedInAccount(this)
         if (!GoogleSignIn.hasPermissions(GoogleSignIn.getLastSignedInAccount(this), fitnessOptions)) {
             GoogleSignIn.requestPermissions(
                     this, // your activity
@@ -36,40 +35,53 @@ class MainActivity : AppCompatActivity() {
                     GoogleSignIn.getLastSignedInAccount(this),
                     fitnessOptions)
         } else {
-            accessGoogleFit()
+            subscribe()
         }
     }
 
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         if (resultCode == Activity.RESULT_OK) {
             if (requestCode == GOOGLE_FIT_PERMISSIONS_REQUEST_CODE) {
-                accessGoogleFit()
+                subscribe()
             }
         }
     }
 
-    private fun accessGoogleFit() {
-        val cal = Calendar.getInstance()
-        var endTime: Long = 0
-        var startTime: Long = 0
-        cal.apply {
-            time = Date()
-            endTime = timeInMillis
-            add(Calendar.YEAR, -1)
-            startTime = timeInMillis
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem?): Boolean {
+        return when (item?.itemId) {
+            R.id.sync -> {
+                readData()
+                true
+            }
+            else -> {
+                super.onOptionsItemSelected(item)
+            }
         }
+    }
 
-        val readRequest: DataReadRequest = DataReadRequest.Builder()
-                .aggregate(DataType.TYPE_STEP_COUNT_DELTA, DataType.AGGREGATE_STEP_COUNT_DELTA)
-                .setTimeRange(startTime, endTime, TimeUnit.MICROSECONDS)
-                .bucketByTime(1, TimeUnit.HOURS)
-                .build()
+    private fun subscribe() {
+        Fitness.getRecordingClient(this, GoogleSignIn.getLastSignedInAccount(this))
+                .subscribe(DataType.TYPE_ACTIVITY_SAMPLES)
+                .addOnSuccessListener {
+                    Log.d(TAG, "Successfully subscribed!")
+                }
+                .addOnFailureListener { e ->
+                    Log.d(TAG, e.stackTrace.toString())
+                }
+    }
 
-        val response = Fitness.getHistoryClient(this, GoogleSignIn.getLastSignedInAccount(this))
-                .readData(readRequest)
-
-        val readDataResult = Tasks.await(response)
-        val dataSet = readDataResult.getDataSet(DataType.TYPE_STEP_COUNT_DELTA)
-        Log.d("dataSet", dataSet?.toString())
+    private fun readData() {
+        Fitness.getHistoryClient(this, GoogleSignIn.getLastSignedInAccount(this))
+                .readDailyTotal(DataType.TYPE_STEP_COUNT_DELTA)
+                .addOnSuccessListener { dataSet ->
+                    val total = if (dataSet.isEmpty) 0 else dataSet.dataPoints[0].getValue(Field.FIELD_STEPS).asInt()
+                    Log.d("total: ", total.toString())
+                }
+                .addOnFailureListener { e -> Log.e("err", e.stackTrace.toString()) }
     }
 }
